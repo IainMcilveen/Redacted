95f2fd0e1c1a4bd75b5053d31593c26f1415360a
Jeremy Bankes <jeremy.bankes@gmail.com>
2026-01-31 15:33:11 -0400

    Glass cracking.

--- src/environment.rs
+++ src/environment.rs
@@ -1,144 +1,67 @@
-use std::f32::consts::PI;
-use std::ops::Div;
-
-use bevy::asset::LoadedFolder;
 use bevy::image::{ImageAddressMode, ImageLoaderSettings, ImageSampler, ImageSamplerDescriptor};
-use bevy::math::Affine2;
 use bevy::prelude::*;
 use bevy_sprite3d::{Sprite3d, Sprite3dPlugin};
 
 use super::GameState;
-use crate::loading::GlassCracksFolder;
+use crate::loading::GameAssets;
 
 pub const CAMERA_POS: Vec3 = Vec3::new(0.0, 1.75, 0.0);
 
+#[derive(Resource)]
+struct ImageList {
+    glass_crack_images: Vec<Handle<Image>>,
+}
+
+#[derive(Component)]
+struct GlassCrackWall;
+
+#[derive(Resource)]
+struct GlassCrackStage(usize);
+
+#[derive(Resource)]
+struct TemporaryTimer(Timer);
+
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(Sprite3dPlugin)
+        .add_systems(Update, update_glass_cracks)
         .add_systems(OnEnter(GameState::PAGETEST), setup);
-}
 
-fn create_texture_atlas(
-    folder: &LoadedFolder,
-    padding: Option<UVec2>,
-    sampling: Option<ImageSampler>,
-    textures: &mut ResMut<Assets<Image>>,
-) -> (TextureAtlasLayout, TextureAtlasSources, Handle<Image>) {
-    println!("Creating texture atlas!");
-    let mut texture_atlas_builder = TextureAtlasBuilder::default();
-    texture_atlas_builder.padding(padding.unwrap_or_default());
-    for handle in folder.handles.iter() {
-        let id = handle.id().typed_unchecked::<Image>();
-        let Some(texture) = textures.get(id) else {
-            warn!(
-                "{} did not resolve to an `Image` asset.",
-                handle.path().unwrap()
-            );
-            continue;
-        };
-        println!("Some Texture: {id}");
-        texture_atlas_builder.add_texture(Some(id), texture);
-    }
+    app.insert_resource(GlassCrackStage(0));
 
-    let result = texture_atlas_builder.build();
-    if result.is_err() {
-        let error_result = result.err().unwrap();
-        println!("Error {error_result}")
-    }
-
-    let (texture_atlas_layout, texture_atlas_sources, texture) =
-        texture_atlas_builder.build().unwrap();
-
-    let texture = textures.add(texture);
-
-    // Update the sampling settings of the texture atlas
-    let image = textures.get_mut(&texture).unwrap();
-    image.sampler = sampling.unwrap_or_default();
-
-    (texture_atlas_layout, texture_atlas_sources, texture)
-}
-
-fn load_textures(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Load multiple, individual sprites from a folder
-    // commands.insert_resource(RpgSpriteFolder(asset_server.load_folder("textures/rpg")));
+    app.insert_resource(TemporaryTimer(Timer::from_seconds(
+        0.5,
+        TimerMode::Repeating,
+    )));
 }
 
 fn setup(
     mut commands: Commands,
-    asset_server: Res<AssetServer>,
+    assets: Res<GameAssets>,
     mut meshes: ResMut<Assets<Mesh>>,
-    glass_cracks_handles: Res<GlassCracksFolder>,
-    loaded_folders: Res<Assets<LoadedFolder>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut textures: ResMut<Assets<Image>>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let (glass_cracks_layout, _sources, atlas_image) = create_texture_atlas(
-        loaded_folders.get(&glass_cracks_handles.0).unwrap(),
-        Some(UVec2::splat(2)),
-        None,
-        &mut textures,
-    );
-
-    let glass_cracks_layout_handle = texture_atlas_layouts.add(glass_cracks_layout);
-
-    // let glass_cracks_material_handle = materials.add(StandardMaterial {
-    //     base_color_texture: Some(atlas_image.clone()),
-    //     unlit: true,
-    //     ..default()
-    // });
-
-    let wall_texture_handle =
-        asset_server.load_with_settings("textures/wall.png", |settings: &mut _| {
-            *settings = ImageLoaderSettings {
-                sampler: ImageSampler::Descriptor(ImageSamplerDescriptor {
-                    address_mode_u: ImageAddressMode::Repeat,
-                    address_mode_v: ImageAddressMode::Repeat,
-
-                    ..default()
-                }),
-                ..default()
-            }
-        });
-
-    let wall_material_handle = materials.add(StandardMaterial {
-        base_color_texture: Some(wall_texture_handle.clone()),
-        uv_transform: Affine2::from_scale(Vec2::new(1.0, -1.0)),
-        alpha_mode: AlphaMode::Blend,
-        unlit: true,
-        ..default()
-    });
-
-    // Wall
-    // commands.spawn((
-    //     Mesh3d(meshes.add(Plane3d::new(Vec3::NEG_Z, Vec2::new(16.0, 12.0).div(2.0)).mesh())),
-    //     MeshMaterial3d(wall_material_handle),
-    //     Transform::from_xyz(0.0, 2.0, 10.0),
-    // ));
-
     commands.spawn((
-        Sprite::from_atlas_image(
-            atlas_image,
-            TextureAtlas {
-                layout: glass_cracks_layout_handle,
-                index: 0,
-            },
-        ),
+        Sprite::from_image(assets.wall.clone()),
         Sprite3d {
             pixels_per_metre: 40.0,
             alpha_mode: AlphaMode::Blend,
             unlit: true,
-            // pivot: Some(Vec2::new(0.5, 0.5)),
             ..default()
         },
         Transform::from_xyz(0.0, 0.0, 10.0),
     ));
 
-    // Glass Cracks
-    // commands.spawn((
-    //     Mesh3d(meshes.add(Plane3d::new(Vec3::NEG_Z, Vec2::new(16.0, 12.0).div(2.0)).mesh())),
-    //     MeshMaterial3d(glass_cracks_material_handle),
-    //     Transform::from_xyz(0.0, 2.0, 10.0),
-    // ));
+    commands.spawn((
+        Sprite::from_image(assets.glass_cracks[0].clone()),
+        Sprite3d {
+            pixels_per_metre: 40.0,
+            alpha_mode: AlphaMode::Blend,
+            unlit: true,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 0.0, 10.0),
+        GlassCrackWall,
+    ));
 
     // Desk
     commands.spawn((
@@ -164,4 +87,19 @@ fn setup(
         // Page View
         Transform::from_xyz(0.0, 1.75, 0.0).looking_at(Vec3::new(0.0, 1.75, 1.0), Vec3::Y), //Transform::from_xyz(0.0, 1.0, 3.0).looking_at(Vec3::Y, Vec3::Y),
     ));
+}
+
+fn update_glass_cracks(
+    time: Res<Time>,
+    mut timer: ResMut<TemporaryTimer>,
+    mut glass_crack_stage: ResMut<GlassCrackStage>,
+    mut query: Query<&mut Sprite, With<GlassCrackWall>>,
+    assets: Res<GameAssets>,
+) {
+    if timer.0.tick(time.delta()).just_finished() {
+        glass_crack_stage.0 = (glass_crack_stage.0 + 1) % 11;
+    }
+    for mut sprite in &mut query {
+        sprite.image = assets.glass_cracks[glass_crack_stage.0].clone();
+    }
 }
--- src/loading.rs
+++ src/loading.rs
@@ -1,31 +1,60 @@
-use bevy::{asset::LoadedFolder, image::ImageSampler, prelude::*};
+use bevy::prelude::*;
 
 use crate::GameState;
 
-#[derive(Resource, Default)]
-pub struct GlassCracksFolder(pub Handle<LoadedFolder>);
+#[derive(Resource, Default, Debug)]
+pub struct GameAssets {
+    pub wall: Handle<Image>,
+    pub glass_cracks: Vec<Handle<Image>>,
+}
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(GameState::LOADING), load)
-        .add_systems(Update, check_textures.run_if(in_state(GameState::LOADING)));
+    app.add_systems(Startup, load)
+        .add_systems(Update, check_ready.run_if(in_state(GameState::LOADING)));
 }
 
 fn load(mut commands: Commands, asset_server: Res<AssetServer>) {
-    println!("Loading...");
-    let glass_cracks_folder = asset_server.load_folder("textures/glass_cracks");
-    commands.insert_resource(GlassCracksFolder(glass_cracks_folder));
+    commands.insert_resource(GameAssets {
+        wall: asset_server.load("textures/wall.png"),
+        glass_cracks: vec![
+            asset_server.load("textures/glass_cracks/glass_cracks1.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks2.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks3.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks4.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks5.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks6.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks7.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks8.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks9.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks10.png"),
+            asset_server.load("textures/glass_cracks/glass_cracks11.png")
+        ],
+    });
 }
 
-fn check_textures(
+fn check_ready(
     mut next_state: ResMut<NextState<GameState>>,
-    rpg_sprite_folder: Res<GlassCracksFolder>,
-    mut events: MessageReader<AssetEvent<LoadedFolder>>,
+    asset_server: Res<AssetServer>,
+    assets: Res<GameAssets>,
 ) {
-    // Advance the `AppState` once all sprite handles have been loaded by the `AssetServer`
-    for event in events.read() {
-        if event.is_loaded_with_dependencies(&rpg_sprite_folder.0) {
-            println!("Moving to game state...");
-            next_state.set(GameState::PAGETEST);
-        }
+    if asset_server
+        .get_load_state(assets.wall.id())
+        .is_some_and(|asset| asset.is_loaded())
+    {
+        next_state.set(GameState::PAGETEST);
     }
 }
+
+// fn check_textures(
+//     mut next_state: ResMut<NextState<GameState>>,
+//     rpg_sprite_folder: Res<GlassCracksFolder>,
+//     mut events: MessageReader<AssetEvent<LoadedFolder>>,
+// ) {
+//     // Advance the `AppState` once all sprite handles have been loaded by the `AssetServer`
+//     for event in events.read() {
+//         if event.is_loaded_with_dependencies(&rpg_sprite_folder.0) {
+//             println!("Moving to game state...");
+//             next_state.set(GameState::PAGETEST);
+//         }
+//     }
+// }
